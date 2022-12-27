024e36143a728af2380048e7e615a08cd7ae5875
GoisNeto <3611208+goisneto@users.noreply.github.com>
2022-12-27 10:25:14 -0300

    Fix Termux-Scripts-Utils-link2bin.sh

--- Termux-Scripts-Utils-link2bin.sh
+++ Termux-Scripts-Utils-link2bin.sh
@@ -3,9 +3,11 @@ if [ -n "$PREFIX" ] && [ -d "$PREFIX" ]; then
 	mypath="$( realpath "${BASH_SOURCE[0]}" )"
 	if [ ! -f "$mypath" ]; then
 	        mypath="$( realpath "$0" )"
+	else
+		echo ok
 	fi
 	if [ -f "$mypath" ]; then
 		mydir="$(dirname "$mypath")"
-		ln -s "${mydir}/*" "$PREFIX/bin/"
+		find "$mydir" -executable -exec ln -s "{}" "$PREFIX/bin/" \;
 	fi
 fi
